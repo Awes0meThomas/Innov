4664ebb781042c8ad8eb599db468437e3312fbe7
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-17 16:25:44 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,36 +1,44 @@
+#!/usr/bin/env python3
+#-- coding: utf-8 --
 import RPi.GPIO as GPIO
 import time
 
 
-GPIO.setmode(GPIO.BOARD)
+#Set function to calculate percent from angle
+def angle_to_percent (angle) :
+    if angle > 180 or angle < 0 :
+        return False
+
+    start = 4
+    end = 12.5
+    ratio = (end - start)/180 #Calcul ratio from angle to percent
+
+    angle_as_percent = angle * ratio
+
+    return start + angle_as_percent
 
 
-servo_pin = 12
+GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
+GPIO.setwarnings(False) #Disable warnings
 
+#Use pin 12 for PWM signal
+pwm_gpio = 12
+frequence = 50
+GPIO.setup(pwm_gpio, GPIO.OUT)
+pwm = GPIO.PWM(pwm_gpio, frequence)
 
-GPIO.setup(servo_pin, GPIO.OUT)
+#Init at 0°
+pwm.start(angle_to_percent(0))
+time.sleep(1)
 
+#Go at 90°
+pwm.ChangeDutyCycle(angle_to_percent(90))
+time.sleep(1)
 
+#Finish at 180°
+pwm.ChangeDutyCycle(angle_to_percent(180))
+time.sleep(1)
 
-pwm = GPIO.PWM(servo_pin, 50)  
-
-
-def set_angle(angle):
-    duty_cycle = (angle / 18) + 2  
-    pwm.ChangeDutyCycle(duty_cycle)
-    time.sleep(1)  
-
-
-try:
-    while True:
-        set_angle(0)  
-        time.sleep(1)
-        set_angle(90)  
-        time.sleep(1)
-        set_angle(180)  
-        time.sleep(1)
-
-except KeyboardInterrupt:
-
-    pwm.stop()
-    GPIO.cleanup()
+#Close GPIO & cleanup
+pwm.stop()
+GPIO.cleanup()
