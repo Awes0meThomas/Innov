c23d6374611466829776cb3badab6c92381afe97
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-17 17:12:38 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,15 +1,14 @@
 import RPi.GPIO as GPIO
 import time
-import RPi.GPIO as GPIO
 from gpiozero import Servo
 
-# Configurer le GPIO pour le capteur infrarouge
-IR_PIN = 7  # GPIO17 (ou le numéro de votre choix)
+
+IR_PIN = 7  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(IR_PIN, GPIO.IN)
 
-# Configurer le GPIO pour le servomoteur
-SERVO_PIN = 18  # GPIO18 (ou le numéro de votre choix)
+
+SERVO_PIN = 18  
 servo = Servo(SERVO_PIN)
 
 try:
@@ -18,10 +17,10 @@ try:
            print ("Mouvement detecte")
            time.sleep(1)
            print("Rotation du servomoteur de 70 degrés.")
-           servo.value = 0.5  # Rotation du servomoteur de 70 degrés (valeur à ajuster si nécessaire)
+           servo.value = 0.5  
            time.sleep(1)
            print("Retour du servomoteur à la position d'origine.")
-           servo.value = 0  # Retour à la position d'origine
+           servo.value = 0  
        else:
            time.sleep(0.1)
 
