293b214b8564fca7f9eb723cd7c33b367b4ecb2c
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-18 12:55:17 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,26 +1,39 @@
 import RPi.GPIO as GPIO
 import time
 
+# Configuration des broches du capteur infrarouge et du servo moteur
+IR_SENSOR_PIN = 8  # Remplacez par le numéro de la broche du capteur infrarouge
+SERVO_PIN = 18      # Remplacez par le numéro de la broche du servo moteur
 
+# Configuration du servo moteur
+PWM_FREQUENCY = 50  # Fréquence PWM en Hz
+DUTY_CYCLE_START = 2.5  # Cycle de service initial (angle de 0 degrés)
+DUTY_CYCLE_END = 12.5   # Cycle de service final (angle de 180 degrés)
+
+# Configuration du GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)  
-GPIO.setup(17, GPIO.OUT)  
+GPIO.setup(IR_SENSOR_PIN, GPIO.IN)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
 
+# Configuration du PWM pour le servo moteur
+pwm = GPIO.PWM(SERVO_PIN, PWM_FREQUENCY)
 
-def rotate_servo(angle):
-    GPIO.output(17, True)
-    time.sleep(angle / 180)
-    GPIO.output(17, False)
+try:
+    while True:
+        if GPIO.input(IR_SENSOR_PIN) == GPIO.HIGH:
+            print("Objet détecté !")
+            time.sleep(1)  # Attendre 1 seconde
+            pwm.start(DUTY_CYCLE_START)  # Tourner de 70 degrés (angle de départ)
+            time.sleep(1)  # Attendre 1 seconde
+            pwm.ChangeDutyCycle(DUTY_CYCLE_END)  # Retourner à la position d'origine (angle de 0 degrés)
+            time.sleep(1)  # Attendre 1 seconde
+            pwm.ChangeDutyCycle(DUTY_CYCLE_START)  # Réinitialiser la position (angle de départ)
+        else:
+            time.sleep(0.1)  # Attendre un court instant avant de vérifier à nouveau
 
-# Boucle principale
-while True:
-    # Si le capteur infrarouge détecte quelque chose
-    if GPIO.input(23) == GPIO.HIGH:
-        # Attendre une seconde
-        time.sleep(1)
+except KeyboardInterrupt:
+    pass
 
-        # Rotation du servo moteur de 70 degrés
-        rotate_servo(70)
-
-        # Retour à la position d'origine
-        rotate_servo(0)
+# Nettoyage des broches GPIO et arrêt du PWM
+pwm.stop()
+GPIO.cleanup()
