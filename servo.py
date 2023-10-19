754b2ff738a1e6433a3c22a5a5d5257bcc258eac
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-19 11:39:14 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-IR_SENSOR_PIN = 8  # Remplacez par le numéro de la broche du capteur infrarouge
-SERVO_PIN = 18      # Remplacez par le numéro de la broche du servo moteur
+IR_SENSOR_PIN = 8  
+SERVO_PIN = 18      
 
 PWM_FREQUENCY = 50
 DUTY_CYCLE_START = 2.5
@@ -22,18 +22,18 @@ try:
             print("Objet détecté !")
             object_detected = True
 
-            # Attendre 1 seconde
+           
             time.sleep(1)
 
-            # Tourner de 70 degrés (angle de départ)
+            
             pwm.start(DUTY_CYCLE_START)
             time.sleep(1)
 
-            # Retourner à la position d'origine (angle de 0 degrés)
+           
             pwm.ChangeDutyCycle(DUTY_CYCLE_END)
             time.sleep(1)
 
-            # Réinitialiser la position (angle de départ)
+           
             pwm.ChangeDutyCycle(DUTY_CYCLE_START)
             object_detected = False
         else:
