e6f1951dfb934971d93ac3fbe4d2699b60e05c87
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-20 12:27:23 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-IR_SENSOR_PIN = 8  
-SERVO_PIN = 18      
+IR_EMITTER_PIN = 17  
+PHOTODETECTOR_PIN = 18 
+SERVO_PIN = 19  
 
 PWM_FREQUENCY = 50
 DUTY_CYCLE_START = 2.5
 DUTY_CYCLE_END = 12.5
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(IR_SENSOR_PIN, GPIO.IN)
+GPIO.setup(IR_EMITTER_PIN, GPIO.OUT)
+GPIO.setup(PHOTODETECTOR_PIN, GPIO.IN)
 GPIO.setup(SERVO_PIN, GPIO.OUT)
 
 pwm = GPIO.PWM(SERVO_PIN, PWM_FREQUENCY)
@@ -18,11 +20,16 @@ object_detected = False
 
 try:
     while True:
-        if GPIO.input(IR_SENSOR_PIN) == GPIO.HIGH and not object_detected:
+       
+        GPIO.output(IR_EMITTER_PIN, GPIO.HIGH)
+        time.sleep(0.1)
+
+       
+        if GPIO.input(PHOTODETECTOR_PIN) == GPIO.HIGH and not object_detected:
             print("Objet détecté !")
             object_detected = True
 
-           
+            
             time.sleep(1)
 
             
@@ -33,11 +40,14 @@ try:
             pwm.ChangeDutyCycle(DUTY_CYCLE_END)
             time.sleep(1)
 
-           
+            
             pwm.ChangeDutyCycle(DUTY_CYCLE_START)
             object_detected = False
-        else:
-            time.sleep(0.1)
+
+       
+        GPIO.output(IR_EMITTER_PIN, GPIO.LOW)
+
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     pass
