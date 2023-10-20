c8974495a67e2bc28d8818c91b851728e0893377
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-20 13:00:43 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 IR_EMITTER_PIN = 17  
-PHOTODETECTOR_PIN = 19
-SERVO_PIN = 18  
+PHOTODETECTOR_PIN = 19  
+SERVO_PIN = 17  
 
 PWM_FREQUENCY = 50
 DUTY_CYCLE_START = 2.5
@@ -17,17 +17,18 @@ GPIO.setup(SERVO_PIN, GPIO.OUT)
 pwm = GPIO.PWM(SERVO_PIN, PWM_FREQUENCY)
 
 object_detected = False
+laser_on = True  
 
 try:
     while True:
-       
-        GPIO.output(IR_EMITTER_PIN, GPIO.HIGH)
-        time.sleep(0.1)
-
-       
-        if GPIO.input(PHOTODETECTOR_PIN) == GPIO.HIGH and not object_detected:
+     
+        GPIO.output(IR_EMITTER_PIN, GPIO.HIGH if laser_on else GPIO.LOW)
+        
+        
+        if GPIO.input(PHOTODETECTOR_PIN) == GPIO.LOW and not object_detected:
             print("Objet détecté !")
             object_detected = True
+            laser_on = False  
 
             
             time.sleep(1)
@@ -43,9 +44,7 @@ try:
             
             pwm.ChangeDutyCycle(DUTY_CYCLE_START)
             object_detected = False
-
-       
-        GPIO.output(IR_EMITTER_PIN, GPIO.LOW)
+            laser_on = True  
 
         time.sleep(0.1)
 
