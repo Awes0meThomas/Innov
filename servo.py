76d5449bb32c249777baa9f1f94c8113a8850623
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-20 13:50:57 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,47 +1,60 @@
 import RPi.GPIO as GPIO
 import time
-from gpiozero import Servo
 
+# Configuration des broches du capteur infrarouge et du servo moteur
+IR_SENSOR_PIN = 19  # Remplacez par le numéro de la broche du capteur infrarouge
+SERVO_PIN = 18      # Remplacez par le numéro de la broche du servo moteur
 
-IR_PIN = 19  
+# Configuration du servo moteur
+PWM_FREQUENCY = 50  # Fréquence PWM en Hz
+DUTY_CYCLE_START = 2.5  # Cycle de service initial (angle de 0 degrés)
+DUTY_CYCLE_END = 12.5   # Cycle de service final (angle de 180 degrés)
+PWM_FREQUENCY = 50
+DUTY_CYCLE_START = 2.5
+DUTY_CYCLE_END = 12.5
+
+# Configuration du GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(IR_PIN, GPIO.IN)
-GPIO.setup(23, GPIO.IN)  
-GPIO.setup(17, GPIO.OUT)  
+GPIO.setup(IR_SENSOR_PIN, GPIO.IN)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
 
+# Configuration du PWM pour le servo moteur
+pwm = GPIO.PWM(SERVO_PIN, PWM_FREQUENCY)
 
-SERVO_PIN = 18  
-servo = Servo(SERVO_PIN)
-def rotate_servo(angle):
-    GPIO.output(17, True)
-    time.sleep(angle / 180)
-    GPIO.output(17, False)
+object_detected = False
 
 try:
     while True:
-       if GPIO.input(capteur):
-           print ("Mouvement detecte")
-           time.sleep(1)
-           print("Rotation du servomoteur de 70 degrés.")
-           servo.value = 0.5  
-           time.sleep(1)
-           print("Retour du servomoteur à la position d'origine.")
-           servo.value = 0  
-       else:
-           time.sleep(0.1)
-# Boucle principale
-while True:
-    # Si le capteur infrarouge détecte quelque chose
-    if GPIO.input(23) == GPIO.HIGH:
-        # Attendre une seconde
-        time.sleep(1)
+        if GPIO.input(IR_SENSOR_PIN) == GPIO.HIGH:
+        if GPIO.input(IR_SENSOR_PIN) == GPIO.HIGH and not object_detected:
+            print("Objet détecté !")
+            time.sleep(1)  # Attendre 1 seconde
+            pwm.start(DUTY_CYCLE_START)  # Tourner de 70 degrés (angle de départ)  # Attendre 1 seconde
+            pwm.ChangeDutyCycle(DUTY_CYCLE_END)  # Retourner à la position d'origine (angle de 0 degrés)  # Attendre 1 seconde
+            pwm.ChangeDutyCycle(DUTY_CYCLE_START)  # Réinitialiser la position (angle de départ)
+            object_detected = True
+
+            # Attendre 1 seconde
+            time.sleep(1)
+
+            # Tourner de 70 degrés (angle de départ)
+            pwm.start(DUTY_CYCLE_START)
+            time.sleep(1)
+
+            # Retourner à la position d'origine (angle de 0 degrés)
+            pwm.ChangeDutyCycle(DUTY_CYCLE_END)
+            time.sleep(1)
+
+            # Réinitialiser la position (angle de départ)
+            pwm.ChangeDutyCycle(DUTY_CYCLE_START)
+            object_detected = False
+        else:
+            time.sleep(3)  # Attendre un court instant avant de vérifier à nouveau
+            time.sleep(0.1)
 
 except KeyboardInterrupt:
-    print("Arrêt du programme")
-        # Rotation du servo moteur de 70 degrés
-        rotate_servo(70)
+    pass
 
-finally:
-    GPIO.cleanup()
-        # Retour à la position d'origine
-        rotate_servo(-70)
+# Nettoyage des broches GPIO et arrêt du PWM
+pwm.stop()
+GPIO.cleanup()
