19ac8e08bd249607b038e04aabd7386fad369ab7
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-20 15:46:13 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,46 +1,31 @@
 import RPi.GPIO as GPIO
 import time
+from gpiozero import Servo
 
-IR_SENSOR_PIN = 17  # Remplacez par le numéro de la broche du capteur infrarouge
-SERVO_PIN = 18  # Remplacez par le numéro de la broche du servo moteur
+# Configuration des broches pour le capteur infrarouge et le servomoteur
+capteur_infrarouge_pin = 17  # Remplacez par le numéro de la broche de votre capteur IR
+servomoteur_pin = 18         # Remplacez par le numéro de la broche de votre servomoteur
 
-PWM_FREQUENCY = 50
-DUTY_CYCLE_START = 2.5
-DUTY_CYCLE_END = 12.5
-
+# Initialisation de la bibliothèque GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(IR_SENSOR_PIN, GPIO.IN)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setup(capteur_infrarouge_pin, GPIO.IN)
 
-pwm = GPIO.PWM(SERVO_PIN, PWM_FREQUENCY)
-
-object_detected = False
+# Configuration du servomoteur
+servo = Servo(servomoteur_pin)
 
 try:
     while True:
-        if GPIO.input(IR_SENSOR_PIN) == GPIO.HIGH and not object_detected:
-            print("Objet détecté !")
-            object_detected = True
+        if GPIO.input(capteur_infrarouge_pin):
+            print("Objet détecté")
+            time.sleep(1)  # Attendez 1 seconde
 
-            # Attendre 1 seconde
+            # Tourner le servomoteur de 70 degrés
+            servo.value = 0.5
             time.sleep(1)
-
-            # Tourner de 70 degrés (angle de départ)
-            pwm.start(DUTY_CYCLE_START)
-            time.sleep(1)
-
-            # Retourner à la position d'origine (angle de 0 degrés)
-            pwm.ChangeDutyCycle(DUTY_CYCLE_END)
-            time.sleep(1)
-
-            # Réinitialiser la position (angle de départ)
-            pwm.ChangeDutyCycle(DUTY_CYCLE_START)
-            object_detected = False
-        else:
-            time.sleep(0.1)
+            servo.value = 0  # Revenir à la position de base (0 degré)
 
 except KeyboardInterrupt:
     pass
 
-pwm.stop()
+# Nettoyer et réinitialiser les broches GPIO
 GPIO.cleanup()
