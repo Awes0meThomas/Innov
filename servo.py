83f52a44f076b378301e57085e58d3460712b0da
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-24 16:56:24 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,37 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 
-# Configuration des broches du capteur
-pin_capteur = 17  # Remplacez 17 par le numéro de la broche que vous utilisez
-
-# Configuration des broches du Raspberry Pi
+pin_capteur = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_capteur, GPIO.IN)
 
-# Configuration des broches du servo moteur
-pin_servo = 18  # Remplacez 18 par le numéro de la broche que vous utilisez
+pin_servo = 18
 GPIO.setup(pin_servo, GPIO.OUT)
 
-# Configuration du PWM pour le servo moteur
-pwm = GPIO.PWM(pin_servo, 50)  # Fréquence de 50 Hz
+pwm = GPIO.PWM(pin_servo, 50)
 pwm.start(0)
 
+servo_position = 0
+
 def tourner_servo(angle):
+    global servo_position
     duty_cycle = 2.5 + (12.5 - 2.5) * angle / 180.0
     pwm.ChangeDutyCycle(duty_cycle)
+    servo_position = angle
     time.sleep(1)
 
 try:
     while True:
         if GPIO.input(pin_capteur) == GPIO.LOW:
-            print("Obstacle détecté!")
-            time.sleep(1)
-            tourner_servo(70) 
-            time.sleep(1)            # Tourne le servo de 70 degrés
-            tourner_servo(0) 
-            time.sleep(1)       #  Reviens à la position initiale
+            if servo_position != 70:
+                tourner_servo(70)
         else:
-            print("Pas d'obstacle détecté.")
+            if servo_position != 0:
+                tourner_servo(0)
         time.sleep(0.1)
 
 except KeyboardInterrupt:
