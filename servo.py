ab625aaa64f68c8887a6dfeee6e46355011262a7
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-24 16:14:39 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,42 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
-def angle_to_percent(angle):
-    if angle > 180 or angle < 0:
-        return False
-    start = 4
-    end = 12.5
-    ratio = (end - start) / 180
-    angle_as_percent = angle * ratio
-    return start + angle_as_percent
+# Configuration des broches du capteur
+pin_capteur = 17  # Remplacez 17 par le numéro de la broche que vous utilisez
+
+# Configuration des broches du Raspberry Pi
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-pwm_gpio = 18
-frequence = 50
-GPIO.setup(pwm_gpio, GPIO.OUT)
-pwm = GPIO.PWM(pwm_gpio, frequence)
-
-pin_capteur = 17
-
 GPIO.setup(pin_capteur, GPIO.IN)
-
-
-pwm.start(angle_to_percent(0))
-time.sleep(1)
 
 try:
     while True:
         if GPIO.input(pin_capteur) == GPIO.LOW:
-            pwm.ChangeDutyCycle(angle_to_percent(70))
-            time.sleep(1)
-            pwm.ChangeDutyCycle(angle_to_percent(0))
-            time.sleep(1)
+            print("Obstacle détecté!")
         else:
-            time.sleep(1)
+            print("Pas d'obstacle détecté.")
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
-    pass
-
-pwm.stop()
-GPIO.cleanup()
+    GPIO.cleanup()
