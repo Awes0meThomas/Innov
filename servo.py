ebce1fa6a173a6386110fa6926682ec42f554d51
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-25 14:44:07 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,7 +1,7 @@
 import time
-import RPi.GPIO as GPIO
 import board
 import neopixel
+import RPi.GPIO as GPIO
 
 # Set up servo motor
 def angle_to_percent(angle):
@@ -28,27 +28,30 @@ GPIO.setup(pin_capteur, GPIO.IN)
 pin_servo = 18
 GPIO.setup(pin_servo, GPIO.OUT)
 
-pwm_servo = GPIO.PWM(pin_servo, 50)
-pwm_servo.start(0)
+pwm = GPIO.PWM(pin_servo, 50)
+pwm.start(0)
+
+
 
 # Set up neopixel
 NUM_LEDS = 144
-DATA_PIN = 21  # Change this to the BCM pin number
+DATA_PIN = board.D23  # Change this to the BCM pin number
 DELAY_MS = 50
-BUTTON_PIN = 2  # Change this to the BCM pin number
+BUTTON_PIN = 23 # Change this to the BCM pin number
 
 pixels = neopixel.NeoPixel(DATA_PIN, NUM_LEDS, auto_write=False)
 
 # Function to check if the servo motor is moving
 def is_servo_moving():
     def tourner_servo(angle):
-        global servo_position
-        duty_cycle = 2.5 + (12.5 - 2.5) * angle / 180.0
-        pwm_servo.ChangeDutyCycle(duty_cycle)
-        servo_position = angle
-        time.sleep(1)
+    global servo_position
+    duty_cycle = 2.5 + (12.5 - 2.5) * angle / 180.0
+    pwm.ChangeDutyCycle(duty_cycle)
+    servo_position = angle
+    time.sleep(1)
 
-    try:
+try:
+    while True:
         if GPIO.input(pin_capteur) == GPIO.LOW:
             if servo_position != 70:
                 tourner_servo(70)
@@ -56,21 +59,28 @@ def is_servo_moving():
         else:
             if servo_position != 0:
                 tourner_servo(0)
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        pass
+        time.sleep(0.1)
+    pass
 
 # Function for the main loop
 def loop():
     error = 0  # Initialize error as 0
 
     # Add code to check if the servo motor is moving and update the error accordingly
-    if GPIO.input(pin_capteur) == GPIO.LOW:
+    if is_servo_moving():
         error = 1
     else:
         error = 0
 
-    if error == 1:
+    if error == 2:
+        for i in range(NUM_LEDS):
+            pixels[i] = (0, 255, 0)  # Green
+        pixels.show()
+        time.sleep(0.2)
+        pixels.fill((0, 0, 0))  # Turn off all LEDs
+        pixels.show()
+        time.sleep(0.2)
+    elif error == 1:
         for i in range(NUM_LEDS):
             pixels[i] = (255, 0, 0)  # Red
         pixels.show()
@@ -86,7 +96,7 @@ def loop():
             time.sleep(DELAY_MS / 1000)  # Convert to seconds
 
 try:
-    pwm_servo.start(angle_to_percent(0))
+    pwm.start(angle_to_percent(0))
     for _ in range(5):
         loop()
         time.sleep(1)
@@ -97,5 +107,5 @@ try:
 except KeyboardInterrupt:
     pass
 
-pwm_servo.stop()
+pwm.stop()
 GPIO.cleanup()
