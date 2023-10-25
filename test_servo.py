703b9424486d40334073f88b76cd757ad84e0efb
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-25 15:30:45 +0200

    Update test_servo.py

--- test_servo.py
+++ test_servo.py
@@ -1,10 +1,1 @@
-import neopixel
-import board
 
-NUM_LEDS = 144
-DATA_PIN = board.D21  # Assurez-vous que c'est le bon pin
-
-pixels = neopixel.NeoPixel(DATA_PIN, NUM_LEDS, auto_write=True)
-
-# Allumez la LED en rouge
-pixels.fill((255, 0, 0))
