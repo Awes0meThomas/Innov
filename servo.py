3775ab9c9eb636e81e617bd00f04201edf1adc60
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2023-10-25 15:14:28 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,9 +1,7 @@
 import time
 import RPi.GPIO as GPIO
-import board
-import neopixel
 
-# Configuration du servo
+# Set up servo motor
 def angle_to_percent(angle):
     if angle > 180 or angle < 0:
         return False
@@ -13,55 +11,101 @@ def angle_to_percent(angle):
     angle_as_percent = angle * ratio
     return start + angle_as_percent
 
-# Configuration des broches
+# Using BCM pin numbers
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# Configuration du servo
-pwm_gpio = 23  # Utilisez le bon numéro de broche BCM
+pwm_gpio = 23  # Change this to the BCM pin number
 frequency = 50
 GPIO.setup(pwm_gpio, GPIO.OUT)
 pwm = GPIO.PWM(pwm_gpio, frequency)
 
-# Configuration du capteur
 pin_capteur = 17
 GPIO.setup(pin_capteur, GPIO.IN)
 
-# Configuration du servo
 pin_servo = 18
 GPIO.setup(pin_servo, GPIO.OUT)
-pwm_servo = GPIO.PWM(pin_servo, 50)
-pwm_servo.start(0)
 
-# Configuration des LED NéoPixel
+pwm = GPIO.PWM(pin_servo, 50)
+pwm.start(0)
+
+
+
+# Set up neopixel
 NUM_LEDS = 144
-DATA_PIN = board.D21  # Utilisez le bon numéro de broche BCM
+DATA_PIN = board.D21  # Change this to the BCM pin number
 DELAY_MS = 50
+BUTTON_PIN = 2  # Change this to the BCM pin number
 
-# Assurez-vous que DATA_PIN est correctement configuré
-pixels = neopixel.NeoPixel(board.D21, NUM_LEDS, auto_write=False)
+pixels = neopixel.NeoPixel(DATA_PIN, NUM_LEDS, auto_write=False)
 
-# Fonction pour tourner le servo
-def tourner_servo(angle):
-    duty_cycle = angle_to_percent(angle)
-    pwm_servo.ChangeDutyCycle(duty_cycle)
-    time.sleep(1)
+# Function to check if the servo motor is moving
+# Function to check if the servo motor is moving
+def is_servo_moving():
+    def tourner_servo(angle):
+        global servo_position
+        duty_cycle = 2.5 + (12.5 - 2.5) * angle / 180.0
+        pwm.ChangeDutyCycle(duty_cycle)
+        servo_position = angle
+        time.sleep(1)
+
+    try:
+        while True:
+            if GPIO.input(pin_capteur) == GPIO.LOW:
+                if servo_position != 70:
+                    tourner_servo(70)
+                time.sleep(1)
+            else:
+                if servo_position != 0:
+                    tourner_servo(0)
+                time.sleep(0.1)  # Corrected indentation here
+    except KeyboardInterrupt:
+        pass
+
+# Function for the main loop
+def loop():
+    error = 0  # Initialize error as 0
+
+    # Add code to check if the servo motor is moving and update the error accordingly
+    if is_servo_moving():
+        error = 1
+    else:
+        error = 0
+
+    if error == 2:
+        for i in range(NUM_LEDS):
+            pixels[i] = (0, 255, 0)  # Green
+        pixels.show()
+        time.sleep(0.2)
+        pixels.fill((0, 0, 0))  # Turn off all LEDs
+        pixels.show()
+        time.sleep(0.2)
+    elif error == 1:
+        for i in range(NUM_LEDS):
+            pixels[i] = (255, 0, 0)  # Red
+        pixels.show()
+        time.sleep(0.2)
+        pixels.fill((0, 0, 0))  # Turn off all LEDs
+        pixels.show()
+        time.sleep(0.2)
+    else:
+        for color in range(256):
+            for i in range(NUM_LEDS):
+                pixels[i] = (color, 255, 255)  # Change the hue
+            pixels.show()
+            time.sleep(DELAY_MS / 1000)  # Convert to seconds
 
 try:
-    pwm_servo.start(angle_to_percent(0))
-    
+    pwm.start(angle_to_percent(0))
+    for _ in range(5):
+        loop()
+        time.sleep(1)
+
     while True:
-        if GPIO.input(pin_capteur) == GPIO.LOW:
-            if servo_position != 70:
-                tourner_servo(70)
-            time.sleep(1)
-        else:
-            if servo_position != 0:
-                tourner_servo(0)
-            time.sleep(0.1)
+        loop()
+
 except KeyboardInterrupt:
     pass
 
-# Nettoyage des broches
-pwm_servo.stop()
+pwm.stop()
 GPIO.cleanup()
