eb82f478e18eea7bc24dcbd8a47412a31a08c01d
Awes0meThomas <84771913+Awes0meThomas@users.noreply.github.com>
2024-04-16 15:43:41 +0200

    Update servo.py

--- servo.py
+++ servo.py
@@ -3,6 +3,23 @@ import numpy as np
 from gpiozero import AngularServo, DigitalInputDevice
 from time import sleep
 import tensorflow as tf 
+from picamera2 import PiCamera2
+from time import sleep
+
+
+# Initialisation de la caméra
+camera = PiCamera2()
+
+# Prévisualisation de la caméra pendant 5 secondes
+camera.start_preview()
+sleep(5)
+
+# Capture de la photo et sauvegarde dans un fichier
+camera.capture('screenshot.jpg')
+
+# Arrêt de la prévisualisation et libération des ressources
+camera.stop_preview()
+camera.close()
 
 pin_capteur = 17
 capteur = DigitalInputDevice(pin_capteur)
